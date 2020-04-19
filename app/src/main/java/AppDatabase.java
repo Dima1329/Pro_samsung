90794b065f50c8c6a8b51e6802a9837bf955c3be
Dima1329 <peyton.b1329@gmail.com>
2020-04-19 15:01:06 +0300

    Начал добавлять сервер

---
+++ app/src/main/java/AppDatabase.java
@@ -0,0 +1,9 @@
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+import com.example.pro_samsung.Question;
+
+@Database(entities = {Question.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+    public abstract EmployeeDao employeeDao();
+}
